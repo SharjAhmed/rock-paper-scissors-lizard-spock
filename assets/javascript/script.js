5f4f251324f07d1fd1c48f87ecaae869be8c25db
SharjAhmed <sharj.ahmed.91@gmail.com>
2023-02-14 20:45:08 +0000

    Added results, updated rules to show upon click of rules button and updated results js to show winner vs loser when checking score

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,8 @@ let choices = ["rock", "paper", "scissors", "lizard", "spock"];
 let button = document.getElementById("button");
 let playerScore = document.getElementById("player-score");
 let computerScore = document.getElementById("computer-score");
+let rules = document.getElementById("rules");
+let span = document.getElementsByClassName("close")[0];
 
 let player;
 let computer;
@@ -19,13 +21,13 @@ window.onload = function () {
     }
 };
 
-//player choice
+//Player Choice
 function selectChoice() {
 
     player = this.id;
     document.getElementById("player-choice").src = `assets/images/${player}.png`;
 
-    //computer choice
+    //Computer Choice
     computer = choices[Math.floor(Math.random() * 5)];
     document.getElementById("computer-choice").src = `assets/images/${computer}.png`;
 
@@ -35,7 +37,7 @@ function selectChoice() {
     let result = document.getElementById('result');
 
 
-    //result check
+    //Result Check
     if (player == computer) {
         result.innerHTML = 'Draw!';
     }
@@ -43,97 +45,113 @@ function selectChoice() {
         if (player == "rock") {
             if (computer == "scissors") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Rock Crushes Scissors!';
             }
             else if (computer == "lizard") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Rock Crushes Lizard!';
             }
             else if (computer == "paper") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Paper Covers Rock!';
             }
             else if (computer == "spock") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Spock Vaporizes Rock!';
+            }
+        }
+
+        else if (player == "paper") {
+            if (computer == "rock") {
+                playerScore.innerText = ++parsedplayerScore;
+                result.innerHTML = 'You Win! Paper Covers Rock!';
+            }
+            else if (computer == "spock") {
+                playerScore.innerText = ++parsedplayerScore;
+                result.innerHTML = 'You Win! Paper Disproves Spock!';
+            }
+            else if (computer == "scissors") {
+                computerScore.innerText = ++parsedcomputerScore;
+                result.innerHTML = 'You Lose! Scissors Cuts Paper!';
+            }
+            else if (computer == "lizard") {
+                computerScore.innerText = ++parsedcomputerScore;
+                result.innerHTML = 'You Lose! Lizard Eats Paper!';
             }
         }
 
         else if (player == "scissors") {
             if (computer == "paper") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Scissors Cuts Paper!';
             }
             else if (computer == "lizard") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Lizard Eats Paper!';
             }
             else if (computer == "rock") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Rock Crushes Scissors!';
             }
             else if (computer == "spock") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
-            }
-        }
-        else if (player == "paper") {
-            if (computer == "rock") {
-                playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
-            }
-            else if (computer == "spock") {
-                playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
-            }
-            else if (computer == "scissors") {
-                computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
-            }
-            else if (computer == "lizard") {
-                computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Spock Smashes Scissors!';
             }
         }
 
         else if (player == "lizard") {
             if (computer == "spock") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Lizard Poisons Spock!';
             }
             else if (computer == "paper") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Lizard Eats Paper!';
             }
             else if (computer == "scissors") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Scissors Decapitates Lizard!';
             }
             else if (computer == "rock") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Rock Crushes Lizard!';
             }
         }
 
         else if (player == "spock") {
             if (computer == "scissors") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Spock Smashes Scissors!';
             }
             else if (computer == "rock") {
                 playerScore.innerText = ++parsedplayerScore;
-                result.innerHTML = 'You Win!';
+                result.innerHTML = 'You Win! Spock Vaporizes Rock!';
             }
             else if (computer == "paper") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Paper Disproves Spock!';
             }
             else if (computer == "lizard") {
                 computerScore.innerText = ++parsedcomputerScore;
-                result.innerHTML = 'You Lose!';
+                result.innerHTML = 'You Lose! Lizard Poisons Spock!';
             }
         }
 
     }
 
 }
+
+//Rules
+button.onclick = function () {
+    rules.style.display = "block";
+};
+
+span.onclick = function () {
+    rules.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == rules) {
+        rules.style.display = "none";
+    }
+};
